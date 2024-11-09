3715aba0d882da7a1ecb27380d0c5ff2a71bda1c
FRIDA LEMKE <frida.lemke@gmx.de>
2024-11-09 14:28:54 +0100

    add styling of task-card

--- src/components/taskCard/TaskCard.js
+++ src/components/taskCard/TaskCard.js
@@ -1,20 +1,39 @@
 import Link from "next/link";
+import styles from "./TaskCard.module.css";
 
 export default function TaskCard({ task, onToggle, onEdit, onDelete }) {
   return (
-    <li className="task-item">
-      <h2>{task.title}</h2>
-      <label>
-        Done:
-        <input
-          type="checkbox"
-          checked={task.completed}
-          onChange={() => onToggle(task._id)}
-        />
-      </label>
-      <button onClick={() => onEdit(task._id)}>Edit</button>
-      <button onClick={() => onDelete(task._id)}>Delete</button>
-      <Link href={`/tasks/${task._id}`}>View Details</Link>
+    <li className={styles.card}>
+      <h2 className={styles.cardTitle}>{task.title}</h2>
+      <Link href={`/tasks/${task._id}`}>
+        <span className={styles.viewDetailsLink}>View Details</span>
+      </Link>
+      <div className={styles.buttonContainer} aria-label="Task Actions">
+        <button
+          className={styles.editButton}
+          onClick={() => onEdit(task._id)}
+          aria-label="Edit task"
+        >
+          Edit
+        </button>
+        <label className={styles.checkboxButton}>
+          <span>Done: </span>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={task.completed}
+            onChange={() => onToggle(task._id)}
+            aria-label="Mark task as completed"
+          />
+        </label>
+        <button
+          onClick={() => onDelete(task._id)}
+          className={styles.deleteButton}
+          aria-label="Delete task"
+        >
+          Delete
+        </button>
+      </div>
     </li>
   );
 }
