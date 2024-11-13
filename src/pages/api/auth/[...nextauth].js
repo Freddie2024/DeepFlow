b8d880e67d2c97e8e5e1bd778e6cdba156e9b40d
FRIDA LEMKE <frida.lemke@gmx.de>
2024-11-13 17:59:50 +0100

    try to fix vercel problem

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
 import dbConnect from "@/src/lib/db/mongoose";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
@@ -10,10 +9,6 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // EmailProvider({
-    //   server: process.env.EMAIL_SERVER,
-    //   from: process.env.EMAIL_FROM,
-    // }),
   ],
   adapter: MongoDBAdapter(dbConnect()),
   session: {
