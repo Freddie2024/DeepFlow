bcf35c999b99761d8e4089c5d8d542983821f7f5
FRIDA LEMKE <frida.lemke@gmx.de>
2024-11-19 16:48:49 +0100

    add shared dropdown-styles

--- src/components/taskSort/TaskSorter.js
+++ src/components/taskSort/TaskSorter.js
@@ -1,15 +1,17 @@
 import { SORT_OPTIONS } from "./sortUtils";
+import styles from "../styles/shared/Dropdown.module.css";
 
 export default function TaskSorter({ onSortChange, currentSort }) {
   return (
-    <div className="sort-controls">
+    <div className={styles.container}>
       <select
+        className={styles.select}
         value={currentSort}
         onChange={(e) => onSortChange(e.target.value)}
       >
-        <option value={SORT_OPTIONS.DEFAULT}>Default (Creation Date)</option>
-        <option value={SORT_OPTIONS.DATE_ASC}>Date (Earliest First)</option>
-        <option value={SORT_OPTIONS.DATE_DESC}>Date (Latest First)</option>
+        <option value={SORT_OPTIONS.DEFAULT}>Created</option>
+        <option value={SORT_OPTIONS.DATE_ASC}>↑ Date</option>
+        <option value={SORT_OPTIONS.DATE_DESC}>↓ Date</option>
       </select>
     </div>
   );
